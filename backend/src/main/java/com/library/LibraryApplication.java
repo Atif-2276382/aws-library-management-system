6b976e3d240dfd512925fd5723af30ce3efa3d35
atif-kma <kloudatif@gmail.com>
2026-06-23 09:06:43 +0530

    logs

--- backend/src/main/java/com/library/LibraryApplication.java
+++ backend/src/main/java/com/library/LibraryApplication.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -15,5 +17,11 @@ public class LibraryApplication {
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
         log.info("Library Management backend started successfully");
+    }
+
+       @EventListener(ApplicationReadyEvent.class)
+    public void startup() {
+        log.info("=== CUSTOM INFO LOG WORKING ===");
+        log.debug("=== CUSTOM DEBUG LOG WORKING ===");
     }
 }
