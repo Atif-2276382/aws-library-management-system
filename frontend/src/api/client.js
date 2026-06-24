e728ae842f0e2dd0153df87b9989e548e5caf40f
atif-kma <kloudatif@gmail.com>
2026-06-24 12:17:59 +0530

    console

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,4 +1,5 @@
 const API_BASE = import.meta.env.VITE_API_URL || '';
+console.log("API_BASE =", API_BASE);
 
 export async function apiRequest(path, options = {}) {
   const token = localStorage.getItem('token');
